5b9f113e4fc2bf107f3d26fe6f82ae67f649254e
Iain Brookshaw <iain.j.brookshaw@gmail.com>
2019-09-20 14:14:26 -0700

    python: expanded the simulator to add the lines and plot boilerplate outline

--- python/odometry.py
+++ python/odometry.py
@@ -10,10 +10,20 @@ import numpy as np
 
 class DiffDriveOdometry:
 
-    def __init__(self, start_pose: tuple):
+    def __init__(self, start_pose: tuple, wheelbase: float) -> None:
+        self.wheelbase = wheelbase
         self._old_pose = start_pose
 
-    def update_pose(self, v_r: float, v_l: float, delta_t: float, epsilon=0.001: float):
+    def update_pose(self, v_l: float, v_r: float, delta_t: float, epsilon=0.001) -> tuple:
+        """
+        Compute the pose of the robot delta_t seconds after the last pose was computed.
+        This is only going to be accurate for small steps of delta_t
+        :param v_r:     float - the linear velocity of the right-hand wheel (m/s)
+        :param v_l:     float - the linear velocity of the left-hand wheel  (m/s)
+        :param delta_t: float - the time-step from the last pose measurement (sec)
+        :param epsilon: float - the error allowable when computing v_r == v_l (m/s)
+        :returns: the new pose (x',y',theta'), which is also set internally as the next _old_pose 
+        """
         if DiffDriveOdometry._compare_v(v_r, v_l, epsilon):
             return (
                 v_r*delta_t * np.cos(self._old_pose[2]),
@@ -34,10 +44,15 @@ class DiffDriveOdometry:
         self._old_pose = (new_x, new_y, new_theta)
         return self._old_pose
 
-        # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def encoder_to_velocity(delta_encoder_tick: int, max_encoder_tick: int, wheel_rad: float) -> float:
+        pass
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # PRIVATE
 
     @staticmethod
-    def _compare_v(v_l: float, v_r: float, epsilon: float):
+    def _compare_v(v_l: float, v_r: float, epsilon: float) -> bool:
         if np.sign(v_l) != np.sign(v_r):
             return False
         if abs(v_l - v_r) < epsilon:
@@ -45,25 +60,21 @@ class DiffDriveOdometry:
         return False
 
     @staticmethod
-    def _compute_r(v_l: float, v_r: float):
+    def _compute_r(v_l: float, v_r: float) -> float:
         pass
 
     @staticmethod
-    def _compute_omega(v_l: float, v_r: float):
+    def _compute_omega(v_l: float, v_r: float) -> float:
         pass
 
     @staticmethod
-    def _compute_icc(r: float, old_pose: tuple):
+    def _compute_icc(r: float, old_pose: tuple) -> tuple:
         pass
 
     @staticmethod
-    def _compute_new_theta(old_pose: tuple, omega: float, delta_t: float):
+    def _compute_new_theta(old_pose: tuple, omega: float, delta_t: float) -> float:
         pass
 
     @staticmethod
-    def _compute_new_corrdinate(old_pose: tuple, icc: tuple, omega: float, delta_t: float):
+    def _compute_new_corrdinate(old_pose: tuple, icc: tuple, omega: float, delta_t: float) -> tuple:
         pass
-
-
-if __name__ == "__main__":
-    pass
---
+++ python/simulator.py
@@ -0,0 +1,161 @@
+#! /usr/bin/env python3
+"""
+Differential Drive Robot: Simulator
+Iain Brookshaw
+21 September 2019
+"""
+from odometry import DiffDriveOdometry as Odo
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class SimulationAnimation:
+    """
+    This is the 2d animated plot that simulated the movement and odometry of
+    a differential drive robot. This class contains the odometry system, the
+    the ground-truth calculation, noise generation, user interface callbacks
+    and the plotting mechanics
+    """
+
+    def __init__(self, start_pose: tuple, wheelbase: float, delta_t: float) -> None:
+        """
+        :param start_pose: the (x,y,theta) original pose of the robot (m, m, rad)
+        :param wheelbase:  the distance between the robot's two wheels (m)
+        :param delta_t:    the simulation time-step (s)
+        """
+
+        self._odo = Odo(start_pose, wheelbase)
+        self._delta_t = delta_t
+
+        self._motor_command_vector = (1, 0)
+        self._left_encoder_true = 0
+        self._right_encoder_true = 0
+        self._left_encoder_noisy = 0
+        self._right_encoder_noisy = 0
+
+        # the plot-able paths for ground truth and estimated
+        self.ground_truth_path_x = [start_pose[0]]
+        self.ground_truth_path_y = [start_pose[1]]
+        self.ground_truth_path_theta = [start_pose[2]]
+        #
+        self.odo_path_x = [start_pose[0]]
+        self.odo_path_y = [start_pose[1]]
+        self.odo_path_theta = [start_pose[2]]
+
+        # Plot data
+        self.fig = None
+        self._plot_lines = None
+        self._ax = None
+
+    # --------------------------------------------------------------------------
+    # Animation Methods
+
+    def update_animation(self) -> list:
+        """
+        This method updates the simulation state (computing new odometry and gt)
+        and updates the plot
+        """
+
+        # compute the true and noisy encoders and convert the noisy values to velocities
+        l_encoder = self._emulate_left_wheel_encoder()
+        l_enc_noisy = l_encoder + \
+            self._generate_encoder_noise(
+                0.1)  # Todo: hardcoded noise magnitude
+        #
+        r_encoder = self._emulate_right_wheel_encoder()
+        r_enc_noisy = r_encoder + \
+            self._generate_encoder_noise(
+                0.1)  # Todo: hardcoded noise magnitude
+        #
+        v_l = self._encoder_to_velocity(self._left_encoder_noisy, l_enc_noisy)
+        v_r = self._encoder_to_velocity(self._right_encoder_noisy, r_enc_noisy)
+
+        # compute and update the odometry estimate
+        odo_pose = self._odo.update_pose(v_l, v_r, self._delta_t)
+        self.odo_path_x.append(odo_pose[0])
+        self.odo_path_y.append(odo_pose[1])
+        self.odo_path_theta.append(odo_pose[2])
+        if self._plot_lines:
+            self._plot_lines[1].set_data(self.odo_path_x, self.odo_path_y)
+
+        # compute and update the ground truth calculation
+        true_pose = self._update_true_pose(l_encoder, r_encoder)
+        self.ground_truth_path_x.append(true_pose[0])
+        self.ground_truth_path_y.append(true_pose[1])
+        self.ground_truth_path_theta.append(true_pose[2])
+        if self._plot_lines:
+            self._plot_lines[0].set_data(
+                self.ground_truth_path_x, self.ground_truth_path_y)
+
+        return self._plot_lines
+
+    def init_animation(self) -> None:
+        self.fig = plt.figure()
+        self._ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
+        self._plot_lines = [
+            self._ax.plot([], [], lw=2, color='b'),  # Ground truth
+            self._ax.plot([], [], lw=2, color='r.')  # Estimated by Odometry
+        ]
+
+        plt.title("Differential Drive Robot: Odometry Simulation")
+        plt.xlabel("Global X (meters)")
+        plt.xlabel("Global Y (meters)")
+        plt.legend()
+
+        for line in self._plot_lines:
+            line.set_data([], [])
+
+        return self._plot_lines
+
+    # --------------------------------------------------------------------------
+    # User robot control keyboard callbacks
+
+    def left_arrow_cb(self) -> None:
+        pass
+
+    def right_arrow_cb(self) -> None:
+        pass
+
+    def forward_arrow_cb(self) -> None:
+        pass
+
+    def back_arrow_cb(self) -> None:
+        pass
+
+    # --------------------------------------------------------------------------
+    # Private Methods
+
+    def _emulate_left_wheel_encoder(self) -> float:
+        return 1.0  # TODO: implement
+
+    def _emulate_right_wheel_encoder(self) -> float:
+        return 1.0  # TODO: implement
+
+    def _encoder_to_velocity(self, old_encoder, new_encoder) -> float:
+        return 0.0  # TODO: implement
+
+    def _generate_encoder_noise(self, magnitude: float) -> float:
+        return 0.0  # TODO: implement
+
+    def _update_true_pose(self, l_enc, r_enc) -> tuple:
+        return (0, 0, 0)  # TODO: implement
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    # default values. TODO: make args
+    start_pose = (0, 0)
+    wheelbase = 0.10
+    delta_t = 0.01
+
+    sim = SimulationAnimation(start_pose, wheelbase, delta_t)
+
+    anim = animation.FuncAnimation(
+        sim.fig,
+        sim.update_animation,
+        init_func=sim.init_animation
+    )
+
+    plt.show()
