bf3e340e7a5e46849186f6fa1b87ce952a6ad811
Iain Brookshaw <iain.j.brookshaw@gmail.com>
2019-09-22 17:04:18 -0700

    docs: packaged python for pip install, some renames and dir refactor. Added proper readme

--- README.md
+++
@@ -1,2 +0,0 @@
-# mobile_robot
-Demonstrations, reports and math showcasing basic mobile robot data
---
+++ diff_sim/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+"""
+Differential Drive Robot: Setup & Install
+Iain Brookshaw
+21 September 2019
+"""
+
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diff_sim",
+    version="0.0.1",
+    author="Iain Brookshaw",
+    author_email="iain.j.brookshaw@gmail.com",
+    description="Simulation of Diff Drive robot odometry",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    scripts=["diff_simulation.py"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: Linux"
+    ],
+    install_requires=[
+        'numpy>=1.17.2',
+        'matplotlib>=3.1.1'
+    ],
+    python_requires='>=3.6'
+)
